0a73fb897f5382a6c04410090f1c378050932efc
dariaCusutura <cusuturadaria@gmail.com>
2024-02-19 10:29:25 +0200

    Build a layout

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,11 +1,30 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-
   return (
-    <p>
-      dudi
-    </p>
-  )
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px, 1fr",
+      }}
+    >
+      <GridItem area="nav" bg="orange.300">
+        nav bar
+      </GridItem>
+      <GridItem area="main" bg="pink.300">
+        main
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="blue.300">
+          aside
+        </GridItem>
+      </Show>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
