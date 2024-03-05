12134fb718839264b90f0561293dfa72cad80402
dariaCusutura <cusuturadaria@gmail.com>
2024-03-05 12:45:04 +0200

    Implementing heart button

--- vite-project/src/components/HeartButton.tsx
+++ vite-project/src/components/HeartButton.tsx
@@ -1,27 +1,41 @@
 import { FaRegHeart, FaHeart } from "react-icons/fa";
 import { IconButton } from "@chakra-ui/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Recipe } from "../hooks/useRecipes";
 import axios from "axios";
+import apiRecipe from "../services/api-recipe";
 
 interface Props {
   recipe: Recipe;
 }
 
-
-const HeartButton = ({recipe} : Props) => {
+const HeartButton = ({ recipe }: Props) => {
   const [liked, setLiked] = useState(false);
+  const [favArray, setFavArray] = useState<number[]>([]);
+  useEffect(() => {
+    apiRecipe
+      .get<number[]>("/recipes/favorites/array")
+      .then((res) => setFavArray(res.data))
+      .catch((err) => console.log(err.message));
+  }, [favArray]);
   const manageClick = async () => {
     setLiked(!liked);
-    await axios.put("http://localhost:3000/liked", 
-    {recipe: recipe._id, liked: liked}, {withCredentials: true})
-    .catch((err) => {console.log(err); console.log(recipe._id)})
-  }
+    await axios
+      .put(
+        "http://localhost:3000/liked",
+        { recipe: recipe._id, liked: liked },
+        { withCredentials: true }
+      )
+      .catch((err) => {
+        console.log(err);
+        console.log(recipe._id);
+      });
+  };
 
   return (
     <IconButton
       aria-label="heart"
-      icon={liked ? <FaHeart /> : <FaRegHeart />}
+      icon={favArray.includes(recipe._id) ? <FaHeart /> : <FaRegHeart />}
       onClick={() => manageClick()}
     />
   );
