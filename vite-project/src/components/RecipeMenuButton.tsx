e0f6889e9e0e9acc3b99d4bc4bba9259e13e787b
dariaCusutura <cusuturadaria@gmail.com>
2024-03-22 15:05:32 +0200

    Delete recipe alert dialog

--- vite-project/src/components/RecipeMenuButton.tsx
+++ vite-project/src/components/RecipeMenuButton.tsx
@@ -1,15 +1,32 @@
-import { Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogOverlay,
+  Button,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  useDisclosure,
+} from "@chakra-ui/react";
 import axios from "axios";
 import toast from "react-hot-toast";
 import { HiOutlineDotsHorizontal } from "react-icons/hi";
 import { Recipe } from "../hooks/useRecipes";
+import React from "react";
 
 interface Props {
   recipe: Recipe;
 }
 
 const RecipeMenuButton = ({ recipe }: Props) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = React.useRef();
+
   const manageDeleteRecipe = async () => {
+    onClose();
     await axios
       .delete(`http://localhost:3000/recipes/${recipe._id}`)
       .catch((err) => console.log(err))
@@ -20,6 +37,7 @@ const RecipeMenuButton = ({ recipe }: Props) => {
         }, 1000);
       });
   };
+
   return (
     <>
       <Menu>
@@ -28,9 +46,30 @@ const RecipeMenuButton = ({ recipe }: Props) => {
         </MenuButton>
         <MenuList marginTop={-2} marginRight={-2}>
           <MenuItem>Edit recipe</MenuItem>
-          <MenuItem onClick={manageDeleteRecipe}>Delete recipe</MenuItem>
+          <MenuItem onClick={onOpen}>Delete recipe</MenuItem>
         </MenuList>
       </Menu>
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogBody marginTop={3} fontSize={20}>
+              Are you sure you want to delete this recipe?
+            </AlertDialogBody>
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                Cancel
+              </Button>
+              <Button colorScheme="red" onClick={manageDeleteRecipe} ml={3}>
+                Delete
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
     </>
   );
 };
